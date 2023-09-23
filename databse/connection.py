5baf6609ccbf2d2f23d01b5377cf0849c95d0bf2
Mobin Ghanbari <79379464+mobinghanbari@users.noreply.github.com>
2023-09-23 09:52:33 +0330

    Update connection.py

--- databse/connection.py
+++ databse/connection.py
@@ -1,38 +1,3 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# from databases import Database
-# from databases import DatabaseURL
-#
-#
-# # DATABASE_URL = "postgresql://postgres:ir.developer.py@localhost/postgres"
-# DATABASE_URL = "postgresql://postgres:ir.developer.py@localhost/postgres"
-#
-# engine = create_engine(DATABASE_URL)
-#
-# Session = sessionmaker(bind=engine)
-#
-# Base = declarative_base()
-#
-#
-# def get_db() -> Session:
-#     db = Session()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-#
-#
-
-
-
-
-
-
-
-
-
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
