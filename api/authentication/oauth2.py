f1224aa429fdc17c1ffdb91e31ebc369b8f582f6
Mobin Ghanbari <79379464+mobinghanbari@users.noreply.github.com>
2023-09-24 23:31:27 +0330

    Update oauth2.py

--- api/authentication/oauth2.py
+++ api/authentication/oauth2.py
@@ -4,6 +4,7 @@ from jose import jwt, JWTError
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime
 
+from sqlalchemy import select
 from starlette import status
 
 from databse import Session, User
@@ -27,26 +28,29 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="UNAUTHORIZED"
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        uuid = payload.get("uuid")
-        user = db.query(User).filter(User.email == username).first()
-        user_uuid_code = user.session_uuid
-        if not uuid == user_uuid_code:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"message": "The Token is not valid for this session"})
-        if username is None:
+async def current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    async with db.begin():
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="UNAUTHORIZED"
+        )
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            username = payload.get("sub")
+            uuid = payload.get("uuid")
+            user = db.query(User).filter(User.email == username).first()
+            user_uuid_code = user.session_uuid
+            if not uuid == user_uuid_code:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"message": "The Token is not valid for this session"})
+            if username is None:
+                raise credentials_exception
+
+        except JWTError:
             raise credentials_exception
 
-    except JWTError:
-        raise credentials_exception
-
-    user = db.query(User).filter(User.email == username).first()
-    if user is None:
-        raise credentials_exception
-    return user
+        pr = select(User).where(User.email == username)
+        statement = await db.execute(pr)
+        user = statement.scalar_one_or_none()
+        if user is None:
+            raise credentials_exception
+        return user
