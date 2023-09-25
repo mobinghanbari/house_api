4ce69b664c84bbe907c720ab6c07e1631bf93d3a
Mobin Ghanbari <79379464+mobinghanbari@users.noreply.github.com>
2023-09-25 09:00:25 +0330

    Update seeder.py

--- api/users/seeder.py
+++ api/users/seeder.py
@@ -3,13 +3,24 @@ from databse import User
 from .hash_engine import Hash
 
 
-def seed_users(db: Session):
-    # Simple user data
-    users = [
-        User(userName="kalan2023", fullName="parsa kalanpay", email="kalan@gmail.com", hashedPassword=Hash.bcrypt("mobin2018"), DoB="04/12/2005", gender="Male"),
-        User(userName="mahanebra", fullName="mahan ebrahimi", email="takam@gmail.com", hashedPassword=Hash.bcrypt("mobin2018"),DoB="04/12/2005", gender="Male"),
-        User(userName="javadspg", fullName="javad nouri", email="jjnn@gmail.com", hashedPassword=Hash.bcrypt("mobin2018"),DoB="04/12/2005", gender="Male"),
-        User(userName="reza111", fullName="reza zabihi", email="booz@gmail.com", hashedPassword=Hash.bcrypt("mobin2018"),DoB="04/12/2005", gender="Male")
-    ]
-    db.add_all(users)
-    db.commit()
+
+
+async def seed_users(db: Session):
+    try:
+        async with db.begin():
+            users =  [
+             User(userName="kalan2023", fullName="parsa kalanpay", email="kalan@gmail.com", hashedPassword=Hash.bcrypt("mobin2018"), DoB="04/12/2005", gender="Male"),
+             User(userName="mahanebra", fullName="mahan ebrahimi", email="takam@gmail.com", hashedPassword=Hash.bcrypt("mobin2018"),DoB="04/12/2005", gender="Male"),
+             User(userName="javadspg", fullName="javad nouri", email="jjnn@gmail.com", hashedPassword=Hash.bcrypt("mobin2018"),DoB="04/12/2005", gender="Male"),
+             User(userName="reza111", fullName="reza zabihi", email="booz@gmail.com", hashedPassword=Hash.bcrypt("mobin2018"),DoB="04/12/2005", gender="Male")
+         ]
+            db.add_all(users)
+            await db.commit()
+    except Exception as e:
+        # Log or print the error message
+        print(f"Error during data seeding: {str(e)}")
+        # Rollback the transaction to avoid partial data insertion
+        await db.rollback()
+    finally:
+        # Close the session to release resources
+        await db.close()
